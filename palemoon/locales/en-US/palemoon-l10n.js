2184cfcc5a36db2e472595e6e845307b72744255
Sandra <sa@asa.com>
2023-04-13 21:39:11 +0300

    init

---
+++ palemoon/locales/en-US/palemoon-l10n.js
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#filter substitution
+
+pref("general.useragent.locale", "ru-RU");
+pref("browser.tabs.closeButtons", 3);
+pref("extensions.blocklist.enabled", false);
+pref("media.webm.enabled", false);
+pref("browser.tabs.showAudioPlayingIcon", false);
+pref("browser.tabs.insertRelatedAfterCurrent", false);
+pref("gfx.direct2d.force-enabled", true);
+pref("media.hardware-video-decoding.force-enabled", true);
